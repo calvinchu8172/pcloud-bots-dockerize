f07ab5d407926ed5b5c0aa77786f1a42b20391f3
CL Shang <clshang@ecoworkinc.com>
2014-06-18 17:17:51 +0800

    [#13556] Add pair protocol templates
    ref #13556 @1h

---
+++ bot_pair_protocol_template.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+
+# PAIR_START_REQUEST % ['DEVICE_ID', 'BOT_ID', 'SESSION_ID']
+PAIR_START_REQUEST = <<EOT
+<message to="%s" type="normal" from="%s" xml:lang="en">
+  <x xmlns="jabber:x:data" type="submit">
+    <title>pair</title>
+    <field type='hidden' var='action'>
+      <value>start</value>
+    </field>
+  </x>
+  <thread>%d</thread>
+</message>
+EOT
+
+# PAIR_COMPLETED_SUCCESS_RESPONSE % ['DEVICE_ID', 'BOT_ID', 'EMAIL', 'SESSION_ID']
+PAIR_COMPLETED_SUCCESS_RESPONSE = <<EOT
+<message to="%s" type="normal" from="%s" xml:lang="en">
+  <x xmlns="jabber:x:data" type="result">
+    <title>pair</title>
+    <field type='hidden' var='action'>
+      <value>completed</value>
+    </field>
+    <field type='hidden' var='email'>
+      <value>%s</value>
+    </field>
+  </x>
+  <thread>%d</thread>
+</message>
+EOT
+
+#PAIR_COMPLETED_FAILURE_RESPONSE % ['DEVICE_ID', 'BOT_ID', 'ERROR_CODE', 'SESSION_ID']
+PAIR_COMPLETED_FAILURE_RESPONSE = <<EOT
+<message to="%s" type="normal" from="%s" xml:lang="en">
+  <x xmlns="jabber:x:data" type="cancel">
+    <title>pair</title>
+    <field type='hidden' var='action'>
+      <value>completed</value>
+    </field>
+    <field type='text-single' var='ERROR_CODE'>
+      <value>%d</value>
+    </field>
+  </x>
+  <thread>%d</thread>
+</message>
+EOT
+
+#PAIR_TIMEOUT_SUCCESS_RESPONSE % ['DEVICE_ID', 'BOT_ID', 'SESSION_ID']
+PAIR_TIMEOUT_SUCCESS_RESPONSE = <<EOT
+<message to="%s" type="normal" from="%s" xml:lang="en">
+  <x xmlns="jabber:x:data" type="result">
+    <title>pair</title>
+    <field type='hidden' var='action'>
+      <value>cancel</value>
+    </field>
+  </x>
+  <thread>%d</thread>
+</message>
+EOT
+
+#PAIR_TIMEOUT_SUCCESS_FAILURE % ['DEVICE_ID', 'BOT_ID', 'ERROR_CODE','SESSION_ID']
+PAIR_TIMEOUT_SUCCESS_FAILURE = <<EOT
+<message to="%s" type="normal" from="%s" xml:lang="en">
+  <x xmlns="jabber:x:data" type="cancel">
+    <title>pair</title>
+    <field type='hidden' var='action'>
+      <value>cancel</value>
+    </field>
+    <field type='text-single' var='ERROR_CODE'>
+      <value>%d</value>
+    </field>
+  </x>
+  <thread>%d</thread>
+</message>
+EOT
