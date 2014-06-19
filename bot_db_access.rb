3af6a3dbfe6b2b7645b011153306cc3aa4409988
CL Shang <clshang@ecoworkinc.com>
2014-06-19 19:05:40 +0800

    [#13556] Build new access method retreive data by id
    * Modify the algorithm of update data
    
    ref #13556 @40m

--- bot_db_access.rb
+++ bot_db_access.rb
@@ -51,9 +51,9 @@ class BotDBAccess
                                             :host     => db_host)
   end
   
-  def close
-    @Client.close if !@Client.nil?
-  end
+  #def close
+  #  @Client.close if !@Client.nil?
+  #end
   
 #=============== Pairing Methods ===============
 #===============================================
@@ -110,6 +110,12 @@ class BotDBAccess
     end
   end
   
+  def db_pairing_session_access_by_id(id = nil)
+    return nil if id.nil?
+    result = PairingSession.find_by(:id => id)
+    return result
+  end
+  
   def db_pairing_session_insert(user_id = nil, device_id = nil)
     return nil if user_id.nil? || device_id.nil?
     
@@ -123,12 +129,13 @@ class BotDBAccess
   end
   
   def db_pairing_session_update(data={})
-    return nil if data.empty? || !data.has_key?(:id) || !data.has_key?(:user_id) || !data.has_key?(:device_id) || !data.has_key?(:status)
+    return nil if data.empty? || !data.has_key?(:id) || (!data.has_key?(:user_id) && !data.has_key?(:device_id) && !data.has_key?(:status))
     
     result = PairingSession.find_by(:id => data[:id])
-    result.update(user_id: data[:user_id])
-    result.update(device_id: data[:device_id])
-    result.update(status: data[:status])
+    result.update(user_id: data[:user_id]) if data.has_key?(:user_id)
+    result.update(device_id: data[:device_id]) if data.has_key?(:device_id)
+    result.update(status: data[:status]) if data.has_key?(:status)
+    result.update(updated_at: DateTime.now)
     
     return !result.nil? ? TRUE : FALSE
   end
@@ -176,13 +183,13 @@ class BotDBAccess
   end
   
   def db_device_update(data={})
-    return nil if data.empty? || !data.has_key?(:id) || !data.has_key?(:serial_number) || !data.has_key?(:mac_address) || !data.has_key?(:model_name) || !data.has_key?(:firmware_version)
+    return nil if data.empty? || !data.has_key?(:id) || (!data.has_key?(:serial_number) && !data.has_key?(:mac_address) && !data.has_key?(:model_name) && !data.has_key?(:firmware_version))
     
     result = Devices.find_by(:id => data[:id])
-    result.update(serial_number: data[:serial_number])
-    result.update(mac_address: data[:mac_address])
-    result.update(model_name: data[:model_name])
-    result.update(firmware_version: data[:firmware_version])
+    result.update(serial_number: data[:serial_number]) if data.has_key?(:serial_number)
+    result.update(mac_address: data[:mac_address]) if data.has_key?(:mac_address)
+    result.update(model_name: data[:model_name]) if data.has_key?(:model_name)
+    result.update(firmware_version: data[:firmware_version]) if data.has_key?(:firmware_version)
     result.update(updated_at: DateTime.now)
     
     return !result.nil? ? TRUE : FALSE
@@ -209,6 +216,12 @@ class BotDBAccess
     end
   end
   
+  def db_device_session_access_by_id(id = nil)
+    return nil if id.nil?
+    result = DeviceSession.find_by(:id => id)
+    return result
+  end
+  
   def db_device_session_insert(data={})
     return nil if data.empty? || !data.has_key?(:device_id) || !data.has_key?(:ip) || !data.has_key?(:xmpp_account) || !data.has_key?(:password)
 
@@ -227,13 +240,13 @@ class BotDBAccess
   end
   
   def db_device_session_update(data={})
-    return nil if data.empty? || !data.has_key?(:id) || !data.has_key?(:device_id) || !data.has_key?(:ip) || !data.has_key?(:xmpp_account) || !data.has_key?(:password)
+    return nil if data.empty? || !data.has_key?(:id) || (!data.has_key?(:device_id) && !data.has_key?(:ip) && !data.has_key?(:xmpp_account) && !data.has_key?(:password))
     
     result = DeviceSession.find_by(:id => data[:id])
-    result.update(device_id: data[:device_id])
-    result.update(ip: data[:ip])
-    result.update(xmpp_account: data[:xmpp_account])
-    result.update(password: data[:password])
+    result.update(device_id: data[:device_id]) if data.has_key?(:device_id)
+    result.update(ip: data[:ip]) if data.has_key?(:ip)
+    result.update(xmpp_account: data[:xmpp_account]) if data.has_key?(:xmpp_account)
+    result.update(password: data[:password]) if data.has_key?(:password)
     result.update(updated_at: DateTime.now)
     
     return !result.nil? ? TRUE : FALSE
