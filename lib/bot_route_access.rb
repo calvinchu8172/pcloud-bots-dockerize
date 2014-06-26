6ebbb9e47ba10c28e5b5d776002e1fac1e66a307
CL Shang <clshang@ecoworkinc.com>
2014-06-26 15:51:38 +0800

    [#13605] Build access methods for AWS Route53
    ref #13605 @6h

---
+++ lib/bot_route_access.rb
@@ -0,0 +1,119 @@
+#!/usr/bin/env ruby
+
+require 'aws-sdk'
+
+ROUTE_CONFIG_FILE = '../config/bot_route_config.yml'
+
+class BotRouteAccess
+  def initialize
+    @Route = nil
+    @zones_list = Array.new
+    config_file = File.join(File.dirname(__FILE__), ROUTE_CONFIG_FILE)
+    config = YAML.load(File.read(config_file))
+    
+    @Route = self.route_connection(config)
+  end
+  
+  def route_connection(config)
+    account = {:access_key_id => config['access_key_id'],
+               :secret_access_key => config['secret_access_key']
+              }
+    
+    return AWS::Route53.new(account)
+  end
+  
+  def find_zone_id(name=nil)
+    return nil if name.nil?
+    
+    zone_id = nil
+    @zones_list.each do |zone|
+      zone_id = zone[:id] if name.downcase == zone[:name].downcase
+    end
+    
+    if zone_id.nil? then
+      list = Array.new
+      zones = @Route.client.list_hosted_zones
+      zones[:hosted_zones].each do |zone|
+        list << {id: zone[:id], name: zone[:name].downcase}
+        zone_id = zone[:id] if name.downcase == zone[:name].downcase
+      end
+      @zone_list = list
+    end
+    
+    return zone_id
+  end
+  
+  def create_record(data={})
+    return nil if data.empty? || !data.has_key?(:host_name) || !data.has_key?(:domain_name) || !data.has_key?(:ip)
+    
+    domain_name = data[:domain_name].downcase
+    host_name = data[:host_name].downcase
+    
+    zone_id = self.find_zone_id(domain_name)
+    isSuccess = nil
+    if !zone_id.nil? then
+      rrsets = @Route.hosted_zones[zone_id].rrsets
+      begin
+        isSuccess = rrsets.create(host_name + '.' + domain_name,
+                                'A',
+                                :ttl => 60,
+                                :resource_records => [{:value => data[:ip]}])
+      rescue AWS::Route53::Errors::InvalidChangeBatch => error
+        isSuccess = TRUE
+      rescue Exception => error
+        puts error
+      end
+    end
+    
+    return !zone_id.nil? && !isSuccess.nil? ? TRUE : FALSE
+  end
+  
+  def update_record(data={})
+    return nil if data.empty? || !data.has_key?(:host_name) || !data.has_key?(:domain_name) || !data.has_key?(:ip)
+    
+    domain_name = data[:domain_name].downcase
+    host_name = data[:host_name].downcase
+    
+    zone_id = self.find_zone_id(domain_name)
+    isSuccess = nil
+    if !zone_id.nil? then
+      rrsets = @Route.hosted_zones[zone_id].rrsets
+      rrset = rrsets[host_name + '.' + domain_name, 'A']
+      rrset.resource_records = [{:value => data[:ip]}]
+      
+      begin
+        isSuccess = rrset.update
+      rescue Exception => error
+        isSuccess = nil
+        puts error
+      end
+    end
+    
+    return !zone_id.nil? && !isSuccess.nil? ? TRUE : FALSE
+  end
+  
+  def delete_record(data={}) 
+    return nil if data.empty? || !data.has_key?(:host_name) || !data.has_key?(:domain_name)
+    
+    domain_name = data[:domain_name].downcase
+    host_name = data[:host_name].downcase
+    
+    zone_id = self.find_zone_id(domain_name)
+    isSuccess = nil
+    if !zone_id.nil? then
+      rrsets = @Route.hosted_zones[zone_id].rrsets
+      rrset = rrsets[host_name + '.' + domain_name, 'A']
+      
+      begin
+        isSuccess = rrset.delete
+      rescue AWS::Core::Resource::NotFound => error
+        isSuccess = TRUE
+        puts error
+      rescue Exception => error
+        isSuccess = nil
+        puts error
+      end
+    end
+    return !zone_id.nil? && !isSuccess.nil? ? TRUE : FALSE
+  end
+end
