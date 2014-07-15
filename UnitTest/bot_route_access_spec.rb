c9e6beb8b50dcfb3c18b2827e6459eeb41792cb2
CL Shang <clshang@ecoworkinc.com>
2014-07-15 13:59:34 +0800

    Modify Unit-Test, resolv DNS record after create & delete

--- UnitTest/bot_route_access_spec.rb
+++ UnitTest/bot_route_access_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/bot_route_access'
 require 'aws-sdk'
+require 'resolv'
 
 describe BotRouteAccess do
   config_file = File.join(File.dirname(__FILE__), ROUTE_CONFIG_FILE)
@@ -16,12 +17,29 @@ describe BotRouteAccess do
   end
   
   let(:route) {BotRouteAccess.new}
+  let(:resolv_i){Resolv::DNS.new(:nameserver => ['168.95.1.1'])}
   
   dns_data = {host_name: 'mytest3', domain_name: 'demo.ecoworkinc.com.', ip: '10.1.1.112'}
+  ipv4 = nil
   
   it "Create DNS record #{dns_data[:host_name]}.#{dns_data[:domain_name]} - #{dns_data[:ip]}" do
     isSuccess = route.create_record(dns_data)
     expect(isSuccess).to be true
+    
+    i = 0
+    while ipv4.nil?
+      begin
+        ipv4 = resolv_i.getaddress(dns_data[:host_name] + '.' + dns_data[:domain_name])
+      rescue Resolv::ResolvError => error
+        ipv4 = nil
+        puts '        ' + error.to_s
+      end
+      sleep(1)
+      i += 1
+      break if i > 120
+    end
+      
+    expect(ipv4).to be_an_instance_of(Resolv::IPv4)
     
     dns_data[:domain_name] = 'demo.ecoworkinc.com'
     isSuccess = route.create_record(dns_data)
@@ -46,8 +64,27 @@ describe BotRouteAccess do
     isSuccess = route.delete_record(dns_data)
     expect(isSuccess).to be true
     
+    i = 0
+    while !ipv4.nil?
+      begin
+        ipv4 = resolv_i.getaddress(dns_data[:host_name] + '.' + dns_data[:domain_name])
+      rescue Resolv::ResolvError => error
+        ipv4 = nil
+        puts '        ' + error.to_s
+      end
+      sleep(1)
+      i += 1
+      break if i > 120
+    end
+      
+    expect(ipv4).to be_nil
+    
     dns_data[:domain_name] = 'demo.ecoworkinc.com'
     isSuccess = route.delete_record(dns_data)
     expect(isSuccess).to be false
+  end
+  
+  it 'Close DNS resolv' do
+    resolv_i.close  
   end
 end
