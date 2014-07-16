eed46e032162b90d56a798d7a1a5c6bd099db548
CL Shang <clshang@ecoworkinc.com>
2014-07-16 11:00:49 +0800

    [#13822] Build mail access module
    ref #13822 @1.5h

---
+++ lib/bot_mail_content_template.rb
@@ -0,0 +1,7 @@
+FROM_ADDRESS = 'no-reply@pcloud.ecoworkinc.com'
+
+OFFLINE_SUBJECT = 'ZyXEL DDNS Offline Alert'
+OFFLINE_MESSAGE = 'ZyXEL DDNS server is currently offline temporarily and we apologize for the inconvenience.'
+
+ONLINE_SUBJECT = 'ZyXEL DDNS Online Alert'
+ONLINE_MESSAGE = 'ZyXEL DDNS server has return online. Again, we apologize for the inconvenience.'
