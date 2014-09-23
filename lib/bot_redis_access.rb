a24c15fb5a40f33bb86a56fe44490998426d76ad
CL Shang <clshang@ecoworkinc.com>
2014-09-23 12:10:15 +0800

    [#14220] Build redis access methods for session data
    ref #14220 @7.3h

---
+++ lib/bot_redis_access.rb
@@ -0,0 +1,237 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'yaml'
+require 'redis'
+
+REDIS_CONFIG_FILE = '../config/bot_redis_config.yml'
+
+DEVICE_SESSION_KEY = "device:%d:session"
+PAIRING_SESSION_KEY = "device:%d:pairingsession"
+UPNP_SESSION_KEY = "upnp:%d:session"
+DDNS_SESSION_KEY = "ddns:%d:session"
+
+class BotRedisAccess
+
+  def initialize
+    @redis = nil
+    config_file = File.join(File.dirname(__FILE__), REDIS_CONFIG_FILE)
+    config = YAML.load(File.read(config_file))
+    @redis = Redis.new(:host => config['rd_host'], :port => config['rd_port'], :db => config['rd_db'])
+  end
+
+#=============== Pairing Methods ===============
+#===============================================
+  def rd_pairing_session_access(device_id = nil)
+    return nil if nil == device_id
+
+    key = PAIRING_SESSION_KEY % device_id
+    result = @redis.hgetall(key)
+    if !result.empty? then
+      return result
+    else
+      return nil
+    end
+  end
+
+  def rd_pairing_session_insert(data={})
+    return nil if data.empty? || !data.has_key?(:device_id) || (!data.has_key?(:user_id) && !data.has_key?(:status) && !data.has_key?(:start_expire_at) && !data.has_key?(:waiting_expire_at))
+
+    key = PAIRING_SESSION_KEY % data[:device_id]
+
+    @redis.hset(key, "user_id", data[:user_id]) if data.has_key?(:user_id)
+    @redis.hset(key, "status", data[:status]) if data.has_key?(:status)
+    @redis.hset(key, "start_expire_at", data[:start_expire_at]) if data.has_key?(:start_expire_at)
+    @redis.hset(key, "waiting_expire_at", data[:waiting_expire_at]) if data.has_key?(:waiting_expire_at)
+
+    return @redis.hgetall(key)
+  end
+
+  def rd_pairing_session_update(data={})
+    return nil if data.empty? || !data.has_key?(:device_id) || (!data.has_key?(:user_id) && !data.has_key?(:status) && !data.has_key?(:start_expire_at) && !data.has_key?(:waiting_expire_at))
+
+    key = PAIRING_SESSION_KEY % data[:device_id]
+
+    @redis.hset(key, "user_id", data[:user_id]) if data.has_key?(:user_id)
+    @redis.hset(key, "status", data[:status]) if data.has_key?(:status)
+    @redis.hset(key, "start_expire_at", data[:start_expire_at]) if data.has_key?(:start_expire_at)
+    @redis.hset(key, "waiting_expire_at", data[:waiting_expire_at]) if data.has_key?(:waiting_expire_at)
+
+    return TRUE
+  end
+
+  def rd_pairing_session_delete(device_id = nil)
+    return nil if nil == device_id
+    key = PAIRING_SESSION_KEY % device_id
+    hash_key = ["user_id", "status", "start_expire_at", "waiting_expire_at"]
+
+    hash_key.each do |item|
+      @redis.hdel(key, item)
+    end
+
+    return TRUE
+  end
+
+#=============== Device Methods ================
+#===============================================
+  def rd_device_session_access(device_id= nil)
+    return nil if nil == device_id
+
+    key = DEVICE_SESSION_KEY % device_id
+
+    result = @redis.hgetall(key)
+    if !result.empty? then
+      return result
+    else
+      return nil
+    end
+  end
+
+  def rd_device_session_insert(data={})
+    return nil if data.empty? || !data.has_key?(:device_id) || (!data.has_key?(:ip) && !data.has_key?(:xmpp_account))
+
+    key = DEVICE_SESSION_KEY % data[:device_id]
+
+    @redis.hset(key, "ip", data[:ip]) if data.has_key?(:ip)
+    @redis.hset(key, "xmpp_account", data[:xmpp_account]) if data.has_key?(:xmpp_account)
+
+    return @redis.hgetall(key)
+  end
+
+  def rd_device_session_update(data={})
+    return nil if data.empty? || !data.has_key?(:device_id) || (!data.has_key?(:ip) && !data.has_key?(:xmpp_account))
+
+    key = DEVICE_SESSION_KEY % data[:device_id]
+
+    @redis.hset(key, "ip", data[:ip]) if data.has_key?(:ip)
+    @redis.hset(key, "xmpp_account", data[:xmpp_account]) if data.has_key?(:xmpp_account)
+
+    return TRUE
+  end
+
+  def rd_device_session_delete(device_id = nil)
+    return nil if nil == device_id
+
+    key = DEVICE_SESSION_KEY % device_id
+    hash_key = ["ip", "xmpp_account"]
+
+    hash_key.each do |item|
+      @redis.hdel(key, item)
+    end
+
+    return TRUE
+  end
+
+#================ Upnp Methods =================
+#===============================================
+
+  def rd_upnp_session_access(index = nil)
+    return nil if nil == index
+
+    key = UPNP_SESSION_KEY % index
+
+    result = @redis.hgetall(key)
+    if !result.empty? then
+      return result
+    else
+      return nil
+    end
+  end
+
+  def rd_upnp_session_insert(data={})
+    return nil if data.empty? || !data.has_key?(:index) || (!data.has_key?(:user_id) && !data.has_key?(:device_id) && !data.has_key?(:status) && !data.has_key?(:service_list))
+
+    key = UPNP_SESSION_KEY % data[:index]
+
+    @redis.hset(key, "user_id", data[:user_id]) if data.has_key?(:user_id)
+    @redis.hset(key, "device_id", data[:device_id]) if data.has_key?(:device_id)
+    @redis.hset(key, "status", data[:status]) if data.has_key?(:status)
+    @redis.hset(key, "service_list", data[:service_list]) if data.has_key?(:service_list)
+
+    return @redis.hgetall(key)
+  end
+
+  def rd_upnp_session_update(data={})
+    return nil if data.empty? || !data.has_key?(:index) || (!data.has_key?(:user_id) && !data.has_key?(:device_id) && !data.has_key?(:status) && !data.has_key?(:service_list))
+
+    key = UPNP_SESSION_KEY % data[:index]
+
+    @redis.hset(key, "user_id", data[:user_id]) if data.has_key?(:user_id)
+    @redis.hset(key, "device_id", data[:device_id]) if data.has_key?(:device_id)
+    @redis.hset(key, "status", data[:status]) if data.has_key?(:status)
+    @redis.hset(key, "service_list", data[:service_list]) if data.has_key?(:service_list)
+
+    return TRUE
+  end
+
+  def rd_upnp_session_delete(index = nil)
+    return nil if nil == index
+
+    key = UPNP_SESSION_KEY % index
+    hash_key = ["user_id", "device_id", "status", "service_list"]
+
+    hash_key.each do |item|
+      @redis.hdel(key, item)
+    end
+
+    return TRUE
+  end
+
+#================ DDNS Methods =================
+#===============================================  
+  def rd_ddns_session_access(index = nil)
+    return nil if nil == index
+
+    key = DDNS_SESSION_KEY % index
+
+    result = @redis.hgetall(key)
+    if !result.empty? then
+      return result
+    else
+      return nil
+    end
+  end
+
+  def rd_ddns_session_insert(data={})
+    return nil if data.empty? || !data.has_key?(:index) || (!data.has_key?(:device_id) && !data.has_key?(:host_name) && !data.has_key?(:domain_name) && !data.has_key?(:status))
+
+    key = DDNS_SESSION_KEY % data[:index]
+
+    @redis.hset(key, "device_id", data[:device_id]) if data.has_key?(:device_id)
+    @redis.hset(key, "host_name", data[:host_name]) if data.has_key?(:host_name)
+    @redis.hset(key, "domain_name", data[:domain_name]) if data.has_key?(:domain_name)
+    @redis.hset(key, "status", data[:status]) if data.has_key?(:status)
+
+    return @redis.hgetall(key)
+  end
+
+  def rd_ddns_session_update(data={})
+    return nil if data.empty? || !data.has_key?(:index) || (!data.has_key?(:device_id) && !data.has_key?(:host_name) && !data.has_key?(:domain_name) && !data.has_key?(:status))
+
+    key = DDNS_SESSION_KEY % data[:index]
+
+    @redis.hset(key, "device_id", data[:device_id]) if data.has_key?(:device_id)
+    @redis.hset(key, "host_name", data[:host_name]) if data.has_key?(:host_name)
+    @redis.hset(key, "domain_name", data[:domain_name]) if data.has_key?(:domain_name)
+    @redis.hset(key, "status", data[:status]) if data.has_key?(:status)
+
+    return TRUE
+  end
+
+  def rd_ddns_session_delete(index=nil)
+    return nil if nil == index
+
+    key = DDNS_SESSION_KEY % index
+    hash_key = ["device_id", "host_name", "domain_name", "status"]
+
+    hash_key.each do |item|
+      @redis.hdel(key, item)
+    end
+
+    return TRUE
+  end
+  
+  def close
+    @redis.quit
+  end
+end
